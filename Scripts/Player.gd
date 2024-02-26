8bcf519cb83fc92d634de4aa13ebd858bef688b0
20451 <radek.kierski@gmail.com>
2024-02-26 12:17:29 +0100

    Added circle going from side to side

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,12 +1,17 @@
 extends CharacterBody2D
 
 var speed = 300
+var travelDistance = 600
+var windowCenter: Vector2
+var direction = 1
 
 func _ready():
-	print('Character loaded')
+	var windowSize: Vector2 = get_viewport_rect().size
+	windowCenter = Vector2(windowSize.x / 2, windowSize.y / 2)
+	position = windowCenter
 
 func _process(delta):
-	position.x += speed * delta
+	position.x += direction * speed * delta
 	
-	if position.x > 1000:
-		position.x = 0
+	if (position.x - windowCenter.x) * direction >= travelDistance:
+		direction *= -1
