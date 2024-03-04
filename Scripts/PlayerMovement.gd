54a0f2a397187ef97ca31ae51ac4f062a588722a
20451 <radek.kierski@gmail.com>
2024-03-04 18:17:46 +0100

    Added target dummy asset and collision

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
-func _process(delta):
+func _physics_process(delta):
 	var movementInput = Input.get_vector("moveLeft", "moveRight", "moveUp", "moveDown")
+	
 	position += movementInput * SPEED * delta
