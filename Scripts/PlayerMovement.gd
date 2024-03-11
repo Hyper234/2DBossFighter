14b1807a200fe3732d5005d80b5bdeb66a886bc0
20451 <radek.kierski@gmail.com>
2024-03-11 12:20:09 +0100

    Made target dummy into static body, added some assets as well

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -5,4 +5,4 @@ const SPEED = 300.0
 func _physics_process(delta):
 	var movementInput = Input.get_vector("moveLeft", "moveRight", "moveUp", "moveDown")
 	
-	position += movementInput * SPEED * delta
+	move_and_collide(movementInput * SPEED * delta)
